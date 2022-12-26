3ed824873936f1871ddbbd5559a58e2270ee8531
enzom-uy <enzom.dev@gmail.com>
2022-12-26 13:33:20 -0300

    IfElse

---
+++ src/com/company/IfElse.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class IfElse {
+    public static void main(String[] args) {
+        int number1 = 2;
+        int number2 = 3;
+        int number3 = 20;
+        double number4 = 13.4d;
+
+        if (number1 > number3) {
+            System.out.println(number1 + " is greater than " + number3);
+        } else {
+            System.out.println(number1 + " is less than " + number3);
+        }
+    }
+}
