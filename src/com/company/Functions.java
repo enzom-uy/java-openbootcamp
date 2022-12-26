d02b24ed040abcf43677bfa22c8ee86b6203b4dd
enzom-uy <enzom.dev@gmail.com>
2022-12-25 22:59:00 -0300

    Functions

---
+++ src/com/company/Functions.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Functions {
+
+    public static void main(String[] args) {
+        helloWorld();
+        helloWorldWithParams("Enzo");
+        helloWorldWithParams("Nicolás");
+    }
+
+    private static void helloWorld() {
+        System.out.println("Hello world");
+        System.out.println("Hello world 2");
+    }
+
+    private static String helloWorldReturnString() {
+        String name = "Enzo";
+         TE VE
+        return name;
+    }
+
+    private static void helloWorldWithParams(String name) {
+        System.out.println("Hello " + name);
+    }
+}
